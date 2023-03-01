6824633c069da25a2694a629db2cdb0a9d6c3943
Miluche <meshkipla@gmail.com>
2023-03-01 22:40:59 +0300

    added grade, and birthdate columns to students table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_01_193300) do
+ActiveRecord::Schema.define(version: 2023_03_01_193714) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
+    t.string "grade"
+    t.string "birthdate"
   end
 
 end
